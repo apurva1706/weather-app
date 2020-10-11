780739b8fe34923ce9e0e07521f7d41a68619b94
apurva1706 <apurva.peri2001@gmail.com>
2020-10-11 19:19:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Weather from './components/weather';
+import Form from './components/form';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const key='1fa84376bbcb82908609616c4186b9b3';
+
+class App extends Component {
+constructor(){
+  super();
+  this.state={
+    city:undefined,
+    temp:undefined,
+    temp_max:undefined,
+    temp_min:undefined,
+    icon:undefined,
+    error:false,
+    description:""
+  };
+  this.weatherIcon={
+    thunderstorm :"wi-thunderstorm",
+    drizzle:"wi-sleet",
+    rain:"wi-storm-showers",
+    snow:"wi-snow",
+    atmosphere:"wi-fog",
+    clear:"wi-day-sunny",
+    clouds:"wi-day-fog"
+  };
 }
 
+tempCelsius(temp){
+  let cel=Math.floor(temp-273.15);
+  return cel;
+}
+
+get_weatherIcons(rangeId){
+  switch(true){
+    case rangeId>=200&&rangeId<=232:
+      this.setState({icon:this.weatherIcon.thunderstorm});
+      break;
+      case rangeId>=300&&rangeId<=321:
+      this.setState({icon:this.weatherIcon.drizzle});
+      break;
+      case rangeId>=500&&rangeId<=531:
+      this.setState({icon:this.weatherIcon.rain});
+      break;
+      case rangeId>=600&&rangeId<=622:
+      this.setState({icon:this.weatherIcon.snow});
+      break;
+      case rangeId>=701&&rangeId<=781:
+      this.setState({icon:this.weatherIcon.atmosphere});
+      break;
+      case rangeId===800:
+        this.setState({icon:this.weatherIcon.clear});
+        break;
+      default:
+      this.setState({icon:this.weatherIcon.clouds});
+  }
+}
+
+getWeather=async(e)=>{
+  e.preventDefault();
+  const city=e.target.elements.city.value;
+  if(city)
+  { 
+  const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`);
+  const response=await api_call.json();
+  console.log(response);
+  this.setState({
+    city:response.name,
+    temp:this.tempCelsius(response.main.temp),
+    temp_max:this.tempCelsius(response.main.temp_max),
+    temp_min:this.tempCelsius(response.main.temp_min),
+    description:response.weather[0].description,
+    error:false
+  });
+  this.get_weatherIcons(response.weather[0].id);
+}
+else
+this.setState({error:true});
+};
+  render() { 
+    return (
+      <div className="App">
+        <Form loadWeather={this.getWeather} error={this.state.error}/>
+      <Weather 
+      city={this.state.city} 
+      temp={this.state.temp} 
+      temp_max={this.state.temp_max} 
+      temp_min={this.state.temp_min} 
+      icon={this.state.icon} 
+      description={this.state.description}/> 
+    </div>
+      );
+  }
+}
+ 
 export default App;
+
+
---
+++ src/components/weather.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+const Weather = (props) => {
+    return ( 
+        <div className="container">
+            <div className="cards pt-4">
+            <h1>{props.city}</h1>
+            <h5 className="py-4">
+            <i className={`wi ${props.icon} display-1`}></i>
+            </h5>
+            {props.temp?<h3 className="py-2">{props.temp}&deg;C</h3>:null}
+            {minmaxTemp(props.temp_min,props.temp_max)}
+            <h2 className="py-3">{props.description}</h2>
+            </div>
+        </div>
+     );
+}
+function minmaxTemp(min,max){
+    if(min&&max)
+    {
+    return(
+    <h3>
+    <span className="px-4">Minimum-{min}&deg;C</span>
+    <span className="px-4">Maximum-{max}&deg;C</span>
+    </h3>
+);
+    }
+}
+ 
+export default Weather;
