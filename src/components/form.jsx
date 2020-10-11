73d65265ea5a55569ab22125feed5efa4aa0c538
apurva1706 <apurva.peri2001@gmail.com>
2020-10-11 21:49:53 +0530

    first commit

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -8,7 +8,7 @@ const Form=(props)=>{
             <form onSubmit={props.loadWeather}>
             <div className="row">
                 <div className="col-md-3 offset-md-2">
-                    <input type="text" className="form-control" name="city" autoComplete="off " placeholder="City"/>
+                    <input type="text" className="form-control" name="city" autoComplete="off" placeholder="City"/>
                 </div>
                 <div className="col-md-3 mt-md-0 py-2 text-md-left">
                     <button className="btn btn-warning">Get Weather</button>
